eb6f0ad84a91f6761cd40c31e58f0337fcc5295e
GwanWooPark <pgw4712@icloud.com>
2021-10-27 16:07:10 +0900

    양방향 연관관계

---
+++ practice/src/main/java/com/practice/JpaMain.java
@@ -0,0 +1,34 @@
+package com.practice;
+
+import com.practice.domain.Order;
+import com.practice.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Order order = new Order();
+//            order.addOrderItem(new OrderItem());
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
---
+++ practice/src/main/java/com/practice/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.practice.domain;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
