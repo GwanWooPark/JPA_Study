881dc86be47934247b3e46f75e4c68c6a88c44d4
GwanWooPark <pgw4712@icloud.com>
2021-10-30 22:02:46 +0900

    Embedded Type

---
+++ valueType/src/main/java/com/valuetype/Member.java
@@ -0,0 +1,54 @@
+package com.valuetype;
+
+import javax.persistence.*;
+
+@Entity
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    // Period
+    @Embedded
+    private Period workPeriod;
+
+    // Address
+    @Embedded
+    private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Period getWorkPeriod() {
+        return workPeriod;
+    }
+
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
